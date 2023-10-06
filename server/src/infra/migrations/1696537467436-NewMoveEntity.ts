6cc876e21b4ff6ae3ffba264214329c38b12853a
Daniel Dietzler <mail@ddietzler.dev>
2023-10-06 11:12:22 +0200

    remove drop table in migration

--- server/src/infra/migrations/1696537467436-NewMoveEntity.ts
+++ server/src/infra/migrations/1696537467436-NewMoveEntity.ts
@@ -1,14 +1,13 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class NewMoveEntity1696537467436 implements MigrationInterface {
-    name = 'NewMoveEntity1696537467436'
+  name = 'NewMoveEntity1696537467436';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "move_history" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "entityId" character varying NOT NULL, "pathType" character varying NOT NULL, "oldPath" character varying NOT NULL, "newPath" character varying, "deletedAt" TIMESTAMP WITH TIME ZONE, CONSTRAINT "PK_af608f132233acf123f2949678d" PRIMARY KEY ("id"))`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "move_history" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "entityId" character varying NOT NULL, "pathType" character varying NOT NULL, "oldPath" character varying NOT NULL, "newPath" character varying, "deletedAt" TIMESTAMP WITH TIME ZONE, CONSTRAINT "PK_af608f132233acf123f2949678d" PRIMARY KEY ("id"))`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "move_history"`);
-    }
-
+  public async down(): Promise<void> {}
 }
