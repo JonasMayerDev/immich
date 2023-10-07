f0cc8cd5a3b32f3b81e2d65ffacf6c1eb23d7f2b
Daniel Dietzler <mail@ddietzler.dev>
2023-10-07 13:56:05 +0200

    fix move mock

--- server/test/repositories/move.repository.mock.ts
+++ server/test/repositories/move.repository.mock.ts
@@ -5,6 +5,7 @@ export const newMoveRepositoryMock = (): jest.Mocked<IMoveRepository> => {
     create: jest.fn(),
     get: jest.fn(),
     getDeletedMoves: jest.fn(),
+    getFailedMoves: jest.fn(),
     update: jest.fn(),
     prune: jest.fn(),
     softDelete: jest.fn(),
